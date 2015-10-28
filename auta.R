7cea4cb09c65ab3ee6f87494583acf6ebb176e92
Milan Polacek <polacek.emilan@gmail.com>
2015-10-28 10:20:33 +0100

    Pridani hlavicky k csv pridani nacitani dat filtrace podle spotřeby

--- auta.R
+++ auta.R
@@ -1,5 +1,6 @@
-auta <- read.csv("~/Scholla/___FEL/21rocnik/DVZ/1dcv/ukola2015auta.csv", header=FALSE);
-auta2 <- auta[!is.na(auta$V1),]
+auta <- read.csv("~/Scholla/___FEL/21rocnik/DVZ/1dcv/ukola2015auta.csv", header=TRUE);
+auta <- read.csv("~/Schule/FEL/21rocnik/DVZ/AutaR2015/ukola2015auta.csv", header=TRUE)
+auta2 <- auta[,]
 auta2 <- auta[!is.na(auta$V1) &&!is.na(auta$V2) &&!is.na(auta$V3) !is.na(auta$V4) &&
 !is.na(auta$V5) &&!is.na(auta$V6) &&!is.na(auta$V7) &&!is.na(auta$V8) &&!is.na(auta$V9),]
 
