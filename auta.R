59c48266e647774ef6de95abc621eab1c238bd43
Milan Polacek <polacek.emilan@gmail.com>
2015-10-28 11:37:46 +0100

    Přidání grafů závislostí hodnotících tabulek

--- auta.R
+++ auta.R
@@ -1,6 +1,31 @@
 auta <- read.csv("~/Scholla/___FEL/21rocnik/DVZ/1dcv/ukola2015auta.csv", header=TRUE);
 auta <- read.csv("~/Schule/FEL/21rocnik/DVZ/AutaR2015/ukola2015auta.csv", header=TRUE)
-auta2 <- auta[,]
-auta2 <- auta[!is.na(auta$V1) &&!is.na(auta$V2) &&!is.na(auta$V3) !is.na(auta$V4) &&
-!is.na(auta$V5) &&!is.na(auta$V6) &&!is.na(auta$V7) &&!is.na(auta$V8) &&!is.na(auta$V9),]
 
+length(auta$carname) # vraci pocet slozek seznamu
+dim(auta) # vypisuje dimenze takulky dat
+summary(auta) # vypis celkoveho prehledu, dle charakteru 
+plot(auta$mpg, auta$weight,xlab = 'mpg', ylab = 'weight', col= 'blue')
+
+auta2 <- auta[auta$mpg<55,]
+auta2 <- auta2[auta2$mpg>5,]
+
+
+plot(auta2$mpg, auta2$weight,xlab = 'mpg', ylab = 'weight', col= 'blue')
+
+
+length(auta$carname) # vraci pocet slozek seznamu
+dim(auta) # vypisuje dimenze takulky dat
+summary(auta) # vypis celkoveho prehledu, dle charakteru 
+
+m <- lm(auta2$weight ~ auta2$mpg) 
+coef(m)
+abline(coef(m), col='red')
+
+plot(auta2$mpg, auta2$horsepower,xlab = 'mpg', ylab = 'horsepower', col= 'blue')
+auta_horse <- auta2[auta2$horsepower < 300,]
+plot(auta_horse$mpg, auta_horse$horsepower,xlab = 'mpg', ylab = 'horsepower', col= 'blue')
+
+plot(auta2$mpg, auta2$acceleration,xlab = 'mpg', ylab = 'acceleration', col= 'red')
+auta_accel <- auta2[auta2$acceleration < 40,]
+auta_accel <- auta_accel[auta_accel$acceleration > 5,]
+plot(auta_accel$mpg, auta_accel$acceleration,xlab = 'mpg', ylab = 'acceleration', col= 'blue')
