054d155d36c27beb8edf571a1774c98178c18bcb
Milhouse <polacek.emilan@gmail.com>
2015-10-28 21:59:24 +0100

    Odstraneni nevalidnich dat v objemu valcu (csv) dodelan skript na zobrazeni dat Dopsan nejaky text ale chce predelat protokol

--- auta.R
+++ auta.R
@@ -1,27 +1,71 @@
 auta <- read.csv("~/Scholla/___FEL/21rocnik/DVZ/1dcv/ukola2015auta.csv", header=TRUE);
 auta <- read.csv("~/Schule/FEL/21rocnik/DVZ/AutaR2015/ukola2015auta.csv", header=TRUE)
 
+
+#---------------bod 1-------------------
 length(auta$carname) # vraci pocet slozek seznamu
+#bod 1 priznaky
 dim(auta) # vypisuje dimenze takulky dat
+
+#----bod 2 chybejici data (teoretickt take bod 1 a 3)
 summary(auta) # vypis celkoveho prehledu, dle charakteru 
+
+#----- bod 3 odlehla mereni (vizualizace take bod 4)
+boxplot(auta$mpg,ylab = 'Miles per Gallon [mpg]',horizontal = TRUE, main='' )
+boxplot(auta$cylinders,ylab = 'Cylindres [-]',horizontal = TRUE,main='' )
+boxplot(auta$horsepower,ylab = 'Power [Horse power]',horizontal = TRUE,main='' )
+boxplot(auta$weight,ylab = 'Weight [lbs]',horizontal = TRUE,main='' )
+boxplot(auta$acceleration,ylab = 'Acceleration [-]',horizontal = TRUE,main='' )
+boxplot(auta$modelyear,ylab = 'Model year [-]',horizontal = TRUE,main='' )
+boxplot(auta$displacement,ylab = 'Displacement [cubic inches]',horizontal = TRUE,main='' )
+
+
+#-----------------------bod 5 modelovali jsme-----------------
+#pred odstranenim (vizualizace tak bod 4)
 plot(auta$mpg, auta$weight,xlab = 'Miles per Gallon [mpg]', ylab = 'Vehicle weight [lbs]', col= 'blue')
 
 auta2 <- auta[auta$mpg<60,]
 auta2 <- auta2[auta2$mpg>5,]
 
-
+#po odstraneni (vizualizace take bod 4)
 plot(auta2$mpg, auta2$weight,xlab = 'Miles per Gallon [mpg]', ylab = 'Vehicle weight [lbs]', col= 'blue')
 
+modelar <- lm(auta2$weight ~ auta2$mpg) 
+coef(modelat)
+abline(coef(modelar), col='red')
+#------- bod 5 je statisticky vyznamna (podle vseho ne)
+anova(modelar)
 
-length(auta$carname) # vraci pocet slozek seznamu
-dim(auta) # vypisuje dimenze takulky dat
-summary(auta) # vypis celkoveho prehledu, dle charakteru 
 
-m <- lm(auta2$weight ~ auta2$mpg) 
-coef(m)
-abline(coef(m), col='red')
 
-hist(auta2$mpg,main = "",xlab = 'Miles per Gallon [mpg]')
+
+
+
+#----------- bod 6 jestli existuje zavislost na necem jinem
+boxplot(auta$mpg ~ auta$modelyear,xlab = 'Model year [-]', ylab = 'Miles per Gallon [mpg]' )
+#zavislost roku vyroby na spotrebe
+boxplot(auta$mpg ~ auta$cylinders,xlab = 'Cylinders[-]', ylab = 'Miles per Gallon [mpg]' )
+#zavislost roku vyroby na pocte valcu
+
+plot(auta$mpg, auta$horsepower,xlab = 'Miles per Gallon [mpg]', ylab = 'Power [Horse power]', col= 'blue')
+#zavislost spotreby na vykonu
+
+plot(auta$mpg, auta$acceleration,xlab = 'Miles per Gallon [mpg]', ylab = 'Acceleration [-]', col= 'blue')
+#zavislost spotreby na zrychlenmi
+modelar <- lm(auta$acceleration ~ auta$mpg) 
+coef(modelar)
+abline(coef(modelar), col='red')
+
+
+
+
+
+
+
+#------------ bordel okolo to byli experimenty mozne taky k vyuziti
+
+mod = lm(auta2$mpg ~ auta2$weight)
+anova(mod)
 
 plot(auta2$mpg, auta2$horsepower,xlab = 'Miles per Gallon [mpg]', ylab = 'Power [Horse power]', col= 'blue')
 auta_horse <- auta2[auta2$horsepower < 300,]
